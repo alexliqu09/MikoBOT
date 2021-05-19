55632c99ea4cd1e630cb9e7ff15a82388a7c87bd
Alex <alexander.lique.l@uni.pe>
2021-05-19 08:55:36 -0500

    Se agregó la primera versión del bot

--- chat.py
+++ chat.py
@@ -4,12 +4,14 @@ import torch
 from model import Net
 from utils import DataProcess
 
+#################################################################################################
+#                             Cargamos el modelo  entrenado                                     #
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 with open("../MIKOBOT/intents.json", "r", encoding="utf-8") as f:
     intents = json.load(f)
 
-FILE ="data.pth"
+FILE = "data.pth"
 data = torch.load(FILE)
 preprocess_dataset = DataProcess()
 
@@ -19,12 +21,24 @@ output_size = data["ouput_size"]
 all_words = data["all_words"]
 tags = data["tags"]
 model_state = data["model_state"]
+
 model = Net(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
-
+#########################################################################################################
 
 def chat(sentece):
+    """
+    Función:
+        chat:
+            - Esta función permite realizar la inferencia del chatbot.
+
+    Argumentos:
+        sentence:
+            - Se debe agregar un mensaje (str)
+    Retorna:
+            - No retornaré el mensaje realizado por el bot.
+    """
     bot_name = "Miko"
     sentence = preprocess_dataset.tokenize(sentece)
     x = preprocess_dataset.bag_word(sentence, all_words)
@@ -32,16 +46,15 @@ def chat(sentece):
     x = torch.from_numpy(x).to(device)
     x = x.float()
     output = model(x)
-    _,predictions = torch.max(output,dim=1)
+    _, predictions = torch.max(output, dim=1)
     tag = tags[predictions.item()]
 
-    probs = torch.softmax(output,dim=1)
+    probs = torch.softmax(output, dim=1)
     prob = probs[0][predictions.item()]
 
-    if prob.item()>0.65:
+    if prob.item() > 0.75:
         for intent in intents["intents"]:
             if tag == intent["tag"]:
                 return f"{bot_name}: { random.choice(intent['responses'])}"
     else:
         return f"{bot_name}: Aun estoy aprendiendo su lenguaje..."
-
